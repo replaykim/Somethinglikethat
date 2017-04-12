c60fedab62db02ed28aa05a611ee0a07eba4b839
replaykim <replaykim123@gmail.com>
2017-04-12 15:05:23 +0900

    change gradle

--- src/main/java/com/example/CampustoreApplication.java
+++ src/main/java/com/example/CampustoreApplication.java
@@ -2,9 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CampustoreApplication {
+public class CampustoreApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampustoreApplication.class, args);
